e790f2a4b24a67adb3b26e741fc0383ed450ca82
khanmdasad77 <khanmdasad77@gmail.com>
2024-11-26 01:55:18 +0530

    Asignment solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-
 import React from "react";
 import './../styles/App.css';
+import Home from "./Home";
+import About from "./About";
+import {BrowserRouter} from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
+      <div>
+        <nav >
+          <link to="/">Home</link>
+          <link to="/about">About</link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+
         {/* Do not remove the main div */}
-    </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
