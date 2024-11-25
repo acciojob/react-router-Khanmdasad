8af29ca53488fac146c6ec4ffbb2a2223416a115
khanmdasad77 <khanmdasad77@gmail.com>
2024-11-26 02:31:43 +0530

    Asignment solved try 5

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <>
-        <h1>About Us</h1>
-        <p>This is a sample React Router program.</p>
-    </>
-  )
-}
+    <div>
+      <h1>About Us</h1>
+      <p>This is a sample React Router program.</p>
+    </div>
+  );
+};
 
-export default About
+export default About;
